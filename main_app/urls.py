7c53e40447bd15aaf897275508e65867bc48f268
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-09-19 14:43:10 -0600

    implement stripe integration

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('profile/', views.profile, name='profile'),
     path('accounts/register/', views.register, name='register'),
-
+    path('cart/', views.cart, name='cart'),
+    path('config/', views.stripe_config, name='config'),
+    path('create-checkout-session/', views.create_checkout_session, name='checkout-session'),
+    path('success/', views.success, name='success'),
+    path('cancelled/', views.cancelled, name='cancelled'),
      #path('accounts/register', views.register, name='register'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 from .models import Product, Cart, Photo, Post
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
@@ -11,6 +14,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from botocore.exceptions import ClientError
 import uuid
 import boto3
+import stripe
 from django.dispatch import receiver
 from .forms import ProfileForm, UserForm
 from .models import User
@@ -50,7 +54,6 @@ def update_profile():
 def delete_profile():
   pass
 
-
 def register(request):
 
     if request.method == 'POST':
@@ -82,9 +85,47 @@ def update_photo():
 def delete_photo():
   pass
 
+def cart(request):
+  return render(request, 'cart.html')
+
 def add_to_cart():
   pass
 
 def remove_from_cart():
   pass 
 
+@csrf_exempt
+def stripe_config(request):
+  if request.method == 'GET':
+    stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
+    return JsonResponse(stripe_config, safe=False)
+
+@csrf_exempt
+def create_checkout_session(request):
+  if request.method == 'GET':
+    domain_url = 'http://localhost:8000/'
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    try:
+      checkout_session = stripe.checkout.Session.create(
+        success_url = domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url = domain_url + 'cancelled/',
+        payment_method_types = ['card'],
+        mode = 'payment',
+        line_items = [
+          {
+            'name': 'Used Book',
+            'quantity': 1,
+            'currency': 'usd',
+            'amount': 2000,
+          }
+        ]
+      )
+      return JsonResponse({'sessionId': checkout_session['id']})
+    except Exception as e:
+      return JsonResponse({'error': str(e)})
+
+def success(request):
+  return render(request, 'success.html')
+
+def cancelled(request):
+  return render(request, 'cancelled.html')
