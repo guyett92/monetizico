c3732e20a5bc8dd4cee780b5bf9f5ad6c59de1cb
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-09-22 19:02:22 -0600

    get full CRUD on products and posts

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     path('success/', views.success, name='success'),
     path('cancelled/', views.cancelled, name='cancelled'),
     path('products/create', views.AddProduct.as_view(), name='AddProduct'),
+    path('products/<int:pk>/update', views.UpdateProduct.as_view(), name='UpdateProduct'),
+    path('products/<int:pk>/delete', views.DeleteProduct.as_view(), name='DeleteProduct'),
     path('posts/create', views.AddPost.as_view(), name='AddPost'),
+    path('posts/<int:pk>/update', views.UpdatePost.as_view(), name='UpdatePost'),
+    path('posts/<int:pk>/delete', views.DeletePost.as_view(), name='DeletePost'),
 ]
