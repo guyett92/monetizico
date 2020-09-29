86d2b4d4cc684f02730d1aabf8a8d9b5a467ade9
tiyagallegos <tgallegosslp@gmail.com>
2020-09-28 21:17:58 -0600

    added form and views for email

--- main_app/views.py
+++ main_app/views.py
@@ -219,3 +219,23 @@ def success(request):
 
 def cancelled(request):
   return render(request, 'cancelled.html')
+
+##Django Email
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "email.html", {'form': form})
+
+def successView(request):
+    return HttpResponse('Success! Thank you for your message.')
