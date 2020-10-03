524167b4b217b1f2d2de95571e39bf2286c622eb
guyett92 <aaronguyett@yahoo.com>
2020-10-03 08:21:56 -0700

    configures heroku

--- monetizico/settings.py
+++ monetizico/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import environ
+import django_heroku
+
 
 env = environ.Env()
 environ.Env.read_env()
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
